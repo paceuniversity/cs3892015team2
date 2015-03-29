bba2433f9cda9cc33b943e83603f34367da97fc1
baires77 <saires.bruno@gmail.com>
2015-03-29 01:49:23 -0400

    Set a goal activities
    Exercises list added.

--- app/src/main/java/pace/cs389/team2/quickfitness/FragmentSetGoal.java
+++ app/src/main/java/pace/cs389/team2/quickfitness/FragmentSetGoal.java
@@ -22,18 +22,19 @@ package pace.cs389.team2.quickfitness;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import pace.cs389.team2.quickfitness.adapter.CustomExercisesListAdapter;
 import pace.cs389.team2.quickfitness.adapter.CustomSpinnerAdapter;
+import pace.cs389.team2.quickfitness.model.ExercisesItem;
 import pace.cs389.team2.quickfitness.model.SpinnerNavItem;
 
 
@@ -45,15 +46,24 @@ public class FragmentSetGoal extends Fragment implements ActionBar.OnNavigationL
 
     private CustomSpinnerAdapter mSpinnerAdapter;
     private ArrayList<SpinnerNavItem> mSpinnerList;
-    private ListView mSetGoalCategories;
-    private SpinnerNavItem mNavSpinner;
-    private String[] mListItems = {"Pull Down", "Treadmill", "Spin", "Hammer Press", "Leg Press", "Shoulder Press"};
-    private ArrayAdapter<String> mListAdapter;
+    private RecyclerView mRecyclerView;
+    private List<ExercisesItem> mExercisesList;
+    private ExercisesItem mExercisesItem;
+    private ExercisesItem mExercisesItem2;
+    private ExercisesItem mExercisesItem3;
+    private ExercisesItem mExercisesItem4;
+    private ExercisesItem mExercisesItem5;
+    private ExercisesItem mExercisesItem6;
+    private ExercisesItem mExercisesItem7;
+    private ExercisesItem mExercisesItem8;
+    private ExercisesItem mExercisesItem9;
+    private CustomExercisesListAdapter mExercisesAdapter;
 
     private ActionBar actionBar;
 
     public FragmentSetGoal() {
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -68,19 +78,43 @@ public class FragmentSetGoal extends Fragment implements ActionBar.OnNavigationL
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
 
 
-        View view = inflater.inflate(R.layout.fragment_set_goal, container,
+        View view = inflater.inflate(R.layout.activity_list_exercises, container,
                 false);
 
-        mListAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, mListItems);
-        mSetGoalCategories = (ListView) view.findViewById(R.id.set_goal_list_categories);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.cardList);
+        mRecyclerView.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(llm);
 
-        mSpinnerList = new ArrayList<SpinnerNavItem>();
+        mSpinnerList = new ArrayList<>();
         mSpinnerList.add(new SpinnerNavItem("All Categories"));
         mSpinnerList.add(new SpinnerNavItem("Cardio", R.drawable.circle_tag_spinner_green));
         mSpinnerList.add(new SpinnerNavItem("Endurance", R.drawable.circle_tag_spinner_orange));
         mSpinnerList.add(new SpinnerNavItem("Strength", R.drawable.circle_tag_spinner_purple));
         mSpinnerList.add(new SpinnerNavItem("Weight Loss", R.drawable.circle_tag_spinner_grey_blue));
 
+
+        mExercisesItem = new ExercisesItem("Pull Down", "Exercise Description", R.drawable.wall2);
+        mExercisesItem2 = new ExercisesItem("Treadmill", "Exercise Description", R.drawable.wall2);
+        mExercisesItem3 = new ExercisesItem("Leg Press", "Exercise Description", R.drawable.wall2);
+        mExercisesItem4 = new ExercisesItem("Shoulder Press", "Exercise Description", R.drawable.wall2);
+        mExercisesItem5 = new ExercisesItem("Chest Pull Down", "Exercise Description", R.drawable.wall2);
+        mExercisesItem6 = new ExercisesItem("Back Extension", "Exercise Description", R.drawable.wall2);
+        mExercisesItem7 = new ExercisesItem("Leg Extension", "Exercise Description", R.drawable.wall2);
+        mExercisesItem8 = new ExercisesItem("Back Leg", "Exercise Description", R.drawable.wall2);
+        mExercisesItem9 = new ExercisesItem("Neck Extension", "Exercise Description", R.drawable.wall2);
+
+        mExercisesList = new ArrayList<>();
+        mExercisesList.add(mExercisesItem);
+        mExercisesList.add(mExercisesItem2);
+        mExercisesList.add(mExercisesItem3);
+        mExercisesList.add(mExercisesItem4);
+        mExercisesList.add(mExercisesItem5);
+        mExercisesList.add(mExercisesItem6);
+        mExercisesList.add(mExercisesItem7);
+        mExercisesList.add(mExercisesItem8);
+        mExercisesList.add(mExercisesItem9);
 
         // title drop down adapter
         mSpinnerAdapter = new CustomSpinnerAdapter(getActivity().getApplicationContext(), mSpinnerList);
@@ -89,7 +123,7 @@ public class FragmentSetGoal extends Fragment implements ActionBar.OnNavigationL
         actionBar.setListNavigationCallbacks(mSpinnerAdapter, this);
 
 
-        setListExercisesAdapter(mListItems);
+        setListExercisesAdapter(mExercisesList);
 
         return view;
 
@@ -128,8 +162,8 @@ public class FragmentSetGoal extends Fragment implements ActionBar.OnNavigationL
         return false;
     }
 
-    private void setListExercisesAdapter(String[] mList) {
-        mListAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, mList);
-        mSetGoalCategories.setAdapter(mListAdapter);
+    private void setListExercisesAdapter(List<ExercisesItem> mListAdapter) {
+        mExercisesAdapter = new CustomExercisesListAdapter(mListAdapter);
+        mRecyclerView.setAdapter(mExercisesAdapter);
     }
 }
---
+++ app/src/main/java/pace/cs389/team2/quickfitness/adapter/CustomExercisesListAdapter.java
@@ -0,0 +1,104 @@
+/*
+ *
+ *  * Copyright (C) 2014 The Android Open Source Project
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package pace.cs389.team2.quickfitness.adapter;
+
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+import pace.cs389.team2.quickfitness.R;
+import pace.cs389.team2.quickfitness.model.ExercisesItem;
+
+/**
+ * Created by Luiz on 28/03/2015.
+ */
+public class CustomExercisesListAdapter extends RecyclerView.Adapter<CustomExercisesListAdapter.ExercisesViewHolder> {
+
+    private List<ExercisesItem> mExercisesList;
+    private View itemView;
+
+    public CustomExercisesListAdapter(List<ExercisesItem> contactList) {
+        this.mExercisesList = contactList;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mExercisesList.size();
+    }
+
+    @Override
+    public ExercisesViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        itemView = LayoutInflater.
+                from(viewGroup.getContext()).
+                inflate(R.layout.set_goal_exercises_adapter, viewGroup, false);
+
+        return new ExercisesViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ExercisesViewHolder exercisesViewHolder, int i) {
+        ExercisesItem exercisesItem = mExercisesList.get(i);
+
+        if (exercisesItem.getName().equals("Treadmill")) {
+            exercisesViewHolder.mCardBottomLayout.setBackgroundResource(R.color.material_green);
+        } else if (exercisesItem.getName().equals("Leg Press")) {
+            exercisesViewHolder.mCardBottomLayout.setBackgroundResource(R.color.material_orange);
+        } else {
+            exercisesViewHolder.mCardBottomLayout.setBackgroundResource(R.color.material_blue_grey);
+
+        }
+
+        exercisesViewHolder.mExerciseTitle.setText(exercisesItem.getName());
+        exercisesViewHolder.mExerciseDescription.setText(exercisesItem.getDescription());
+        exercisesViewHolder.mImageExerciseTop.setImageResource(exercisesItem.getIcon());
+
+        //Bitmap icon = BitmapFactory.decodeResource(itemView.getResources(),
+        //        exercisesItem.getIcon());
+
+        //Bitmap mIconResized = Bitmap.createScaledBitmap(icon, 350, 160, true);
+
+        //BitmapResizeUtils.getResizedBitmap(icon, 350, 160);
+
+
+        //exercisesViewHolder.mImageExerciseTop.setImageBitmap(BitmapUtils.decodeSampledBitmapFromResource(itemView.getResources(),
+        //       exercisesItem.getIcon(), 350, 160));
+    }
+
+    public static class ExercisesViewHolder extends RecyclerView.ViewHolder {
+        protected TextView mExerciseTitle;
+        protected TextView mExerciseDescription;
+        protected ImageView mImageExerciseTop;
+        protected LinearLayout mCardBottomLayout;
+
+        public ExercisesViewHolder(View v) {
+            super(v);
+            mExerciseTitle = (TextView) v.findViewById(R.id.txt_exercise_title);
+            mExerciseDescription = (TextView) v.findViewById(R.id.txt_exercise_decription);
+            mImageExerciseTop = (ImageView) v.findViewById(R.id.img_exercise_picture);
+            mCardBottomLayout = (LinearLayout) v.findViewById(R.id.card_bottom_layout);
+        }
+    }
+}
