b679a63a27c8f92542331594e61c564ac1b1cc84
luizhsda10 <luizhsda@gmail.com>
2015-03-29 01:26:46 -0300

    Added exercises list to the Set Goal screen.

---
+++ app/src/main/java/pace/cs389/team2/quickfitness/model/ExercisesItem.java
@@ -0,0 +1,83 @@
+/*
+ *
+ *  * Copyright (C) 2014 The Android Open Source Project
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package pace.cs389.team2.quickfitness.model;
+
+/**
+ * Created by Luiz on 28/03/2015.
+ */
+public class ExercisesItem {
+
+    private int id;
+    private String name;
+    private String description;
+    private int icon;
+    private String videoAnimation;
+
+
+    public ExercisesItem(String name, String description, int icon) {
+        this.name = name;
+        this.description = description;
+        this.icon = icon;
+    }
+
+    public ExercisesItem(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getIcon() {
+        return icon;
+    }
+
+    public void setIcon(int icon) {
+        this.icon = icon;
+    }
+
+    public String getVideoAnimation() {
+        return videoAnimation;
+    }
+
+    public void setVideoAnimation(String videoAnimation) {
+        this.videoAnimation = videoAnimation;
+    }
+}
