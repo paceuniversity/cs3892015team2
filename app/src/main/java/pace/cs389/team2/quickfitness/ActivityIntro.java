effe673ef0d7acce0ecc2961af598bab82c0fbcb
luizhsda10 <luizhsda@gmail.com>
2015-04-14 02:05:32 -0300

    Fixed bugs in code to improve app's performance.

--- app/src/main/java/pace/cs389/team2/quickfitness/ActivityIntro.java
+++ app/src/main/java/pace/cs389/team2/quickfitness/ActivityIntro.java
@@ -24,8 +24,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Toast;
 
 
@@ -35,9 +33,9 @@ public class ActivityIntro extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        /*requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);*/
 
         setContentView(R.layout.activity_intro);
     }
